dfead323749904157e242f6d5b9db4eea4bb3b2d
Hasst <sergey1112345@gmail.com>
2020-12-31 18:38:28 +0200

    created: Products component

---
+++ src/components/Products/Products.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Product from '../Product/Product';
+import './Products.css';
+
+export default function Products() {
+  const products = [1, 2, 3, 4];
+
+  return (
+    <div className="Products">
+      {products.map((product) => (
+        <Product />
+      ))}
+    </div>
+  );
+}
