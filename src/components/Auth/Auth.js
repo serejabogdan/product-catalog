e18d517a0ac819074a7ac9095a0eb225e3fa5776
Hasst <sergey1112345@gmail.com>
2020-12-31 18:24:08 +0200

    added: sign in and sign up logic

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,35 +1,82 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Card, Form, Button} from 'bootstrap-4-react';
 import './Auth.css';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
-export default function Auth(props) {
+import {auth} from '../../firebaseConfig';
+import {connect} from 'react-redux';
+
+import {setCurrentUser} from '../../redux/actions';
+
+function Auth(props) {
   const {isSignUp} = props;
+  const [userData, setUserData] = useState({email: '', password: ''});
+  const history = useHistory();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      props.setCurrentUser(user);
+      if (user) {
+        history.push('/products');
+      }
+    });
+  }, []);
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function signin(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  async function signUpHandleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await signup(userData.email, userData.password);
+      history.push('/products');
+    } catch {
+      console.error('Failed to create an account');
+    }
+  }
+
+  async function signInHandleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await signin(userData.email, userData.password);
+      history.push('/products');
+    } catch {
+      console.error('Failed to log in');
+    }
+  }
+
   return (
     <div className="wrapper">
       <Card>
         <Card.Body>
           <Card.Title>{isSignUp ? 'Sign Up' : 'Sign In'}</Card.Title>
-          <Form
-            onSubmit={
-              isSignUp
-                ? (e) => {
-                    e.preventDefault();
-                    console.log('Sign Up');
-                  }
-                : (e) => {
-                    e.preventDefault();
-                    console.log('Sign In');
-                  }
-            }
-          >
+          <Form onSubmit={isSignUp ? signUpHandleSubmit : signInHandleSubmit}>
             <Form.Group>
               <label htmlFor="email-input">Email address</label>
-              <Form.Input type="email" id="email-input" placeholder="Enter email" />
+              <Form.Input
+                type="email"
+                id="email-input"
+                placeholder="Enter email"
+                value={userData.email}
+                onChange={(e) => setUserData((state) => ({...state, email: e.target.value}))}
+              />
             </Form.Group>
             <Form.Group>
               <label htmlFor="password-imput">Password</label>
-              <Form.Input type="password" id="password-imput" placeholder="Password" />
+              <Form.Input
+                type="password"
+                id="password-imput"
+                placeholder="Password"
+                value={userData.password}
+                onChange={(e) => setUserData((state) => ({...state, password: e.target.value}))}
+              />
             </Form.Group>
             <Form.Group></Form.Group>
             <Button primary type="submit">
@@ -44,3 +91,9 @@ export default function Auth(props) {
     </div>
   );
 }
+
+const mapDispatchToProps = {
+  setCurrentUser
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
