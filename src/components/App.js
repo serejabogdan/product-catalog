d42dd120a3a7c47f34900fa9b773831baa9d56cf
Hasst <sergey1112345@gmail.com>
2020-12-31 18:42:28 +0200

    changed: Product to Products component in the Route

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Auth from './Auth/Auth';
 import AddingProductForm from './AddingProductForm/AddingProductForm';
-import Product from './Product/Product';
+import Products from './Products/Products';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <Auth isSignUp={true} />} />
           <Route path="/signIn" render={() => <Auth isSignUp={false} />} />
-          <Route path="/products" component={Product} />
+          <Route path="/products" component={Products} />
         </Switch>
       </Router>
     </div>
