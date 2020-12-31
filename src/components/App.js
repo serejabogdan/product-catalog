41b2a1513e3965cac6b597071e780446cd490438
Hasst <sergey1112345@gmail.com>
2020-12-31 13:47:40 +0200

    changed: App js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Auth from './Auth/Auth';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App__wrapper">
+        <Auth />
+      </div>
+    </div>
+  );
+}
+
+export default App;
