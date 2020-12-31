268dd6ad056d0358402e65725b69f9099015bdfc
Hasst <sergey1112345@gmail.com>
2020-12-31 13:46:16 +0200

    created: Product card component

---
+++ src/components/Product/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Card, Button} from 'bootstrap-4-react';
+import './Product.css';
+
+export default function Product() {
+  const ingUrl = 'https://chizpizza.com.ua/image/cache/catalog/478/4sira-478x478.jpg';
+
+  return (
+    <div>
+      <Card>
+        <Card.Image src={ingUrl} />
+        <Card.Body>
+          <Card.Title>Карбонара</Card.Title>
+          <Card.Text>Сырный соус, сыр дорблю, моцарелла, чедр, пармезан, итальянские травы.</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <Button className="delete-button" primary>
+            Удалить
+          </Button>
+          <Button primary>Изменить</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
+}
