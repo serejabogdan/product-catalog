b425f1fcedaf0d4cdcd3e4186be8ecd06e7a8942
Hasst <sergey1112345@gmail.com>
2021-01-02 11:54:13 +0200

    added: private route to routes in App component

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,27 @@ import Auth from './Auth/Auth';
 import AddingProductForm from './AddingProductForm/AddingProductForm';
 import Products from './Products/Products';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import {connect} from 'react-redux';
 
-function App() {
+function App({isUserAuth}) {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <PrivateRoute path="/products" isUserAuth={isUserAuth} component={Products} />
           <Route exact path="/" render={() => <Auth isSignUp={true} />} />
           <Route path="/signIn" render={() => <Auth isSignUp={false} />} />
-          <Route path="/products" component={Products} />
         </Switch>
       </Router>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isUserAuth: state.auth.currentUser
+  };
+};
+
+export default connect(mapStateToProps)(App);
