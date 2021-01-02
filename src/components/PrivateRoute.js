a4e91d1aa64d8911ae5e5157847dcc2c74eabf9c
Hasst <sergey1112345@gmail.com>
2021-01-02 11:52:11 +0200

    created: private route

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Redirect, Route} from 'react-router-dom';
+
+export default function PrivateRoute({component: Component, isUserAuth, ...rest}) {
+  return <Route {...rest} render={(props) => (isUserAuth ? <Component {...props} /> : <Redirect to="/" />)} />;
+}
