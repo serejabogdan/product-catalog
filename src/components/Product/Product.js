1581b512441676b469512efde389769320329919
Hasst <sergey1112345@gmail.com>
2021-01-03 23:11:57 +0200

    added: product information display

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,35 @@ import React from 'react';
 import {Card, Button} from 'bootstrap-4-react';
 import './Product.css';
 
-export default function Product({title, price, description, file, date, discount}) {
+export default function Product({productId, title, price, description, file, date, discount}) {
+  const isDiscountValid = date > Date.now();
+
+  function getDiscountDaysLeft() {
+    const oneHour = 3600000;
+    const hoursLeft = Math.floor((date - Date.now()) / oneHour);
+    const daysLeft = Math.floor(hoursLeft / 24);
+    return `До конца акции ${daysLeft} ${pluralType(daysLeft)}`;
+  }
+
+  function pluralType(value) {
+    const dayOne = 'день';
+    const dayTwo = 'дня';
+    const dayThree = 'дней';
+    value = Math.abs(value);
+    value %= 100;
+    if (value >= 5 && value <= 20) {
+      return dayThree;
+    }
+    value %= 10;
+    if (value === 1) {
+      return dayOne;
+    }
+    if (value >= 2 && value <= 4) {
+      return dayTwo;
+    }
+    return dayThree;
+  }
+
   return (
     <div className="Product">
       <Card>
@@ -11,6 +39,16 @@ export default function Product({title, price, description, file, date, discount
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
         </Card.Body>
+        <div className="product__price">
+          {isDiscountValid && (
+            <>
+              <div className="discount-days">{getDiscountDaysLeft()}</div>
+              <div className="discount-persent">{discount}% скидка</div>
+              <span className="discount-price">{price - (price * discount) / 100} грн.</span>
+            </>
+          )}
+          <span className={`current-price ${isDiscountValid && 'discount-active'}`}>{price} грн.</span>
+        </div>
         <Card.Footer>
           <Button className="delete-button" primary>
             Удалить
