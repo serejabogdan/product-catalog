62d866ebecf406200dbff77dba19c4f608977ba7
Hasst <sergey1112345@gmail.com>
2021-01-03 14:33:07 +0200

    added: getting data from database to store and render them

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,16 +2,14 @@ import React from 'react';
 import {Card, Button} from 'bootstrap-4-react';
 import './Product.css';
 
-export default function Product() {
-  const ingUrl = 'https://chizpizza.com.ua/image/cache/catalog/478/4sira-478x478.jpg';
-
+export default function Product({title, price, description, file, date, discount}) {
   return (
     <div className="Product">
       <Card>
-        <Card.Image src={ingUrl} />
+        <Card.Image src={file} />
         <Card.Body>
-          <Card.Title>Карбонара</Card.Title>
-          <Card.Text>Сырный соус, сыр дорблю, моцарелла, чедр, пармезан, итальянские травы.</Card.Text>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
         </Card.Body>
         <Card.Footer>
           <Button className="delete-button" primary>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Product from '../Product/Product';
 import './Products.css';
 
-export default function Products() {
-  const products = [1, 2, 3, 4];
+import {database} from '../../firebaseConfig';
+import {connect} from 'react-redux';
+import {setProductsList} from '../../redux/actions';
 
+function Products({setProductsList, productsList}) {
+  useEffect(() => {
+    const data = database.ref('Products');
+    data.on('value', (snapshot) => {
+      const data = snapshot.val();
+      const productsList = [];
+      for (const product in data) {
+        productsList.push({productId: product, ...data[product]});
+      }
+      setProductsList(productsList);
+      console.log(productsList);
+    });
+  }, []);
   return (
     <div className="Products">
-      {products.map((product) => (
-        <Product key={product} />
-      ))}
+      {productsList.map((product) => {
+        const {title, price, description, file, date, discount} = product;
+        return <Product key={title} title={title} description={description} file={file} />;
+      })}
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    productsList: state.products.productsList
+  };
+};
+
+const mapDispatchToProps = {
+  setProductsList
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
