75b827ca680f284c27fff1abee13d6b9fc753c74
Hasst <sergey1112345@gmail.com>
2021-01-04 16:42:23 +0200

    added: type and action to redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import {SET_CURRENT_USER, SET_PRODUCTS} from './types';
+import {SET_CURRENT_USER, SET_PRODUCTS, SET_ID_SELECTED_PRODUCT} from './types';
 
 export function setCurrentUser(user) {
   return {
@@ -11,5 +11,12 @@ export function setProductsList(products) {
   return {
     type: SET_PRODUCTS,
     payload: products
+  };
+}
+
+export function setIdSelectedProduct(product) {
+  return {
+    type: SET_ID_SELECTED_PRODUCT,
+    payload: product
   };
 }
