989cd3766497f5a30263f5617238b1ca177b4383
Hasst <sergey1112345@gmail.com>
2021-01-05 17:19:48 +0200

    changed: a button display condition

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,12 +3,13 @@ import {Button} from 'bootstrap-4-react/lib/components';
 import {NavLink, useLocation} from 'react-router-dom';
 import './Menu.css';
 import {signOut} from '../../utils/auth';
-import {PATH_PRODUCTS} from '../../utils/constants';
+import {PATH_CHANGE_FORM, PATH_FORM, PATH_PRODUCTS} from '../../utils/constants';
 
 export default function Menu() {
   const {pathname} = useLocation();
   const productsPath = `/${PATH_PRODUCTS}`;
-  const formPath = '/form';
+  const formPath = `/${PATH_FORM}`;
+  const formPaths = [formPath, `/${PATH_CHANGE_FORM}`];
   return (
     <nav className="menu">
       {pathname !== productsPath && (
@@ -16,7 +17,7 @@ export default function Menu() {
           <Button primary>Каталог</Button>
         </NavLink>
       )}
-      {pathname !== formPath && (
+      {!formPaths.includes(pathname) && (
         <NavLink to={formPath} className="menu__link">
           <Button primary>Добавить</Button>
         </NavLink>
